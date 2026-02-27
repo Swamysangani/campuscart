9308df6a803cbe8036cbf7b21c44489b1b766b5e
swamy <swamyspartan@gamil.com>
2026-02-27 11:52:18 +0530

    Updated API to production backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://campuscart-rm2e.onrender.com';
 
 // Helper for authenticated requests
 const authFetch = async (url, options = {}) => {
